1ac98d539b741e211fb366d47492ad6010c7d1bb
Richard Julian <rjulian57@gmail.com>
2017-06-13 20:48:39 -0700

    Update master with migration issue boo

--- tracker/migrations/0009_auto_20170613_1942.py
+++ tracker/migrations/0009_auto_20170613_1942.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tracker', '0008_merge_20170607_2035'),
+        ('tracker', '0007_auto_20170607_2026'),
     ]
 
     operations = [
