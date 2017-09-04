dc0b5b9abc9af601d4ed5065a16ed03f77d65657
fnstronggirl <riotbarbie635@gmail.com>
2017-09-04 16:59:49 -0700

    Added Monster Mini Bands
    Added monster mini band to Bands_type

--- tracker/forms.py
+++ tracker/forms.py
@@ -8,7 +8,7 @@ BOXFREE_CHOICES = (('Free', 'Free'), ('Box', 'Box'))
 SUMO_CHOICES = (('Sumo', 'Sumo'), ('Conventional', 'Conventional'))
 BAR_OPTIONS = (('Straight', 'Straight'), ('Giant Cambered', 'Giant Cambered'), ('Buffalo', 'Buffalo'), ('Bow', 'Bow'), ('Safety Squat Bar', 'Safety Squat Bar'))
 BENCH_BAR_OPTIONS = (('Straight', 'Straight'), ('Cambered', 'Cambered'), ('Bow', 'Bow'), ('Football Bar', 'Football Bar'))
-BAND_TYPE = (('None', 'None'), ('Micro Mini', 'Micro Mini'), ('Mini', 'Mini'), ('Light', 'Light'), ('Average', 'Average'), ('Heavy', 'Heavy'))
+BAND_TYPE = (('None', 'None'), ('Micro Mini', 'Micro Mini'), ('Mini', 'Mini'), ('Monster Mini', 'Monster Mini '), ('Light', 'Light'), ('Average', 'Average'), ('Heavy', 'Heavy'))
 
 class SquatForm(forms.ModelForm):
 	class Meta:
