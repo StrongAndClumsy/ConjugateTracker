360011f5cef087d476556dac4ca46e70f4f2addc
Richard Julian <rjulian57@gmail.com>
2017-12-22 21:05:38 -0800

    Update URLs and add signup button formatting

--- conjugate/urls.py
+++ conjugate/urls.py
@@ -20,6 +20,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('tracker.urls')),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
